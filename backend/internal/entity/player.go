72e0c650f51f2655016a7401d13abc2067d0595e
imuhtadov <imuhtadov@mts.ru>
2025-04-05 03:28:17 +0300

    fix player

--- backend/internal/entity/player.go
+++ backend/internal/entity/player.go
@@ -2,7 +2,7 @@ package entity
 
 type Player struct {
 	ID            string  `json:"id" db:"id"`
-	GroupID       string  `json:"race_id" db:"race_id"`
+	GroupID       string  `json:"group_id" db:"group_id"`
 	Name          string  `json:"name" db:"name"`                       // Имя участника
 	Color         string  `json:"color" db:"color"`                     // Цвет майки
 	Number        string  `json:"number" db:"number"`                   // Номер участника
