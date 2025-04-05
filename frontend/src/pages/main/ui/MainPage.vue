e11e27b9e0ad011434baf96d8637ef56f61654cd
vkurseApp <vkurseapp@yandex.ru>
2025-04-05 08:37:15 +0300

    some changes

--- frontend/src/pages/main/ui/MainPage.vue
+++ frontend/src/pages/main/ui/MainPage.vue
@@ -1,17 +1,6 @@
 <template>
-  <a-flex class="test" justify="center" align="center" vertical>
+  <div>
     <a-button type="primary">Test</a-button>
     <h1>Test</h1>
-  </a-flex>
-  <a-flex class="test" justify="center" align="center" vertical>
-    <a-button type="primary">Test</a-button>
-    <h1>Test</h1>
-  </a-flex>
+  </div>
 </template>
-
-<style lang="scss" scoped>
-.test {
-  width: 100%;
-  height: 100vh;
-}
-</style>
